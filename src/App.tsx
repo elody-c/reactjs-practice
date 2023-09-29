5f5b5af849bc04e7db6028737a21f4b5ffcfdd54
Elody Catinel <ecatinel@Elodys-MacBook-Air.local>
2023-09-29 13:00:37 -0400

    refactor : some cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,13 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import TaskList from './TaskList';
 import HttpHelper from './HttpHelper';
 
-const DATA_SOURCE_URL = 'https://api.github.com/users/elody-c';
+const DATA_SOURCE_URL = 'https://api.github.com/users/elody-cjjjj';
 
 interface GitProfile {
   login: string,
   id: string,
-}
-/*
-type PromiseResolve<T> = (value?: T | PromiseLike<T>) => void;
-type PromiseReject = (error?: Error) => void;
-
-type Callback = (error?: Error, data?: number) => void;
-
-function getData(callback: Callback) {
-  setTimeout(() => {
-    if (Math.random() < 0.25) {
-      callback(new Error('Error in retrieving data.'));
-    }
-    else {
-      const data = Math.random();
-      callback(undefined, data);
-    }
-  }, 500);
-} // getData
-function getDataPromise() {
-  return new Promise((resolve: PromiseResolve<number>, reject: PromiseReject): void => {
-    getData((error, data) => {
-      if (error) reject(error);
-      else resolve(data);
-    });
-  });
-}
-
-async function calldata() {
-  await getDataPromise().then(
-    (data) => { // resolve()
-      console.log('Process 1:', data);
-      return getDataPromise();
-    },
-  );
-}
-*/
+};
 
 function App() {
   const [data, setData] = useState<GitProfile>();
@@ -50,19 +15,19 @@ function App() {
   useEffect(() => {
     // declare the async data fetching function
     const fetchData = async () => {
-      const json = await HttpHelper.request<GitProfile>(DATA_SOURCE_URL,console.log);
+      const json = await HttpHelper.request<GitProfile>(DATA_SOURCE_URL, console.error);
       setData(json);
       // SIMPLE VERSION
       // get the data from the api
       // const response = await fetch(DATA_SOURCE_URL);
       // convert the data to json
       // const json = await response.json() as GitProfile;
-
-      // set state with the result
     };
 
     // call the function
-    fetchData().catch(console.error);
+    fetchData().catch(() => {
+      console.log('error fetchig data');
+    });
   }, []);
   return (
     <div className="App">
